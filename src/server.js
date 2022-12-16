828668099a1b60cc4fe4802930a0ce3df0bfef5c
BrVecchi <bno.vecchi@gmail.com>
2022-12-16 15:04:16 -0300

    chore: adjust code for deploy on render

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Está rodando na porta ${port}!`));
