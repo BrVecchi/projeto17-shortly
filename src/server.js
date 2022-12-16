da8687664549fb9492ac7d225b948fc662fbcb17
BrVecchi <bno.vecchi@gmail.com>
2022-12-16 16:47:12 -0300

    add dotenv in server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import router from "../routes/routes.js";
 import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 app.use(express.json());
