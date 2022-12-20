8f7edab0cdd3c45ff0f659a2944ac9e963177977
BrVecchi <bno.vecchi@gmail.com>
2022-12-20 00:48:42 -0300

    feat: url get by id and redirect by shortUrl

--- controllers/urlsController.js
+++ controllers/urlsController.js
@@ -4,10 +4,12 @@ import { nanoid } from "nanoid";
 export const postUrl = async (req, res) => {
   const { url } = req.body;
   const shortUrl = nanoid(6);
-  const token = req.token
+  const token = req.token;
 
   try {
-    const userId = (await connectionDB.query(`SELECT * FROM sessions WHERE token=$1`, [token])).rows[0].userId
+    const userId = (
+      await connectionDB.query(`SELECT * FROM sessions WHERE token=$1`, [token])
+    ).rows[0].userId;
     await connectionDB.query(
       `INSERT INTO urls ("shortUrl", url, "userId") VALUES ($1, $2, $3)`,
       [shortUrl, url, userId]
@@ -18,3 +20,41 @@ export const postUrl = async (req, res) => {
     res.sendStatus(500);
   }
 };
+
+export async function findUrlById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const url = (
+      await connectionDB.query("SELECT * FROM urls WHERE id=$1;", [id])
+    ).rows[0];
+    if (!url) {
+      res.sendStatus(404);
+    }
+    const toSendBody = {
+      id: url.id,
+      shortUrl: url.shortUrl,
+      url: url.url,
+    };
+    res.status(200).send(toSendBody);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function redirectUrl(req, res) {
+  const { shortUrl } = req.params;
+
+  try {
+    const url = (
+      await connectionDB.query(`SELECT * FROM urls WHERE "shortUrl"=$1;`, [shortUrl])
+    ).rows[0];
+    if (!url) {
+      res.sendStatus(404);
+    }
+    const urlToRedirect = url.url
+    res.redirect(urlToRedirect);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- routes/urlsRoutes.js
+++ routes/urlsRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { postUrl } from "../controllers/urlsController.js";
+import { findUrlById, postUrl, redirectUrl } from "../controllers/urlsController.js";
 import { tokenMiddleware } from "../middlewares/tokenValidationMiddleware.js";
 import { urlSchemaValidation } from "../middlewares/urlSchemaMiddleware.js";
 
 export const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", tokenMiddleware, urlSchemaValidation, postUrl);
+urlsRouter.get("/urls/:id", findUrlById);
+urlsRouter.get("/urls/open/:shortUrl", redirectUrl);
+
