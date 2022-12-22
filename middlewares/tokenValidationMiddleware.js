4accc058cd20932ca9605663224939ba5b3227f8
BrVecchi <bno.vecchi@gmail.com>
2022-12-22 20:19:34 -0300

    refactor: token validation

--- middlewares/tokenValidationMiddleware.js
+++ middlewares/tokenValidationMiddleware.js
@@ -1,4 +1,6 @@
-export const tokenMiddleware = (req, res, next) => {
+import { connectionDB } from "../database/db.js";
+
+export const tokenMiddleware = async (req, res, next) => {
     const { authorization } = req.headers;
 
     const token = authorization?.replace("Bearer ", "");
@@ -6,6 +8,17 @@ export const tokenMiddleware = (req, res, next) => {
     if (!token) {
         return res.sendStatus(401);
     }
+     try {
+        const availableTokens = (await connectionDB.query(`SELECT token FROM sessions;`)).rows.map(ta => ta.token)
+        console.log(availableTokens)
+        console.log(token)
+        if (!availableTokens.includes(`${token}`)) {
+            return res.sendStatus(401);
+        }
+     } catch (error) {
+        res.status(500).send(error.message);
+     }
+
     req.token = token
     next()
     }
