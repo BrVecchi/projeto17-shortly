827bdd359e3724ae3278fab709d20ef65e6dc4e5
BrVecchi <bno.vecchi@gmail.com>
2022-12-23 15:36:50 -0300

    refactor: order of ranking

--- controllers/rankController.js
+++ controllers/rankController.js
@@ -8,13 +8,21 @@ export async function getRanking(req, res) {
     FROM users u
     LEFT JOIN urls l ON u.id = l."userId"
     GROUP BY u.id
-    ORDER BY u.id ASC;`)
+    ORDER BY "visitCount" DESC
+    LIMIT 10
+    ;`)
     ).rows;
 
     ranking.map((rank) => {
       if (rank.visitCount === null) {
-        return (rank.visitCount = 0);
+        return (rank.visitCount = "0");
       }
+    });
+
+    ranking.sort(function (a, b) {
+      if (a.visitCount < b.visitCount) return 1;
+      if (a.visitCount > b.visitCount) return -1;
+      return 0;
     });
 
     res.status(200).send(ranking);
