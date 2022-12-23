f0ae10059595f0c2b4fbc7e463568491a57c8319
BrVecchi <bno.vecchi@gmail.com>
2022-12-23 15:20:42 -0300

    refactor: return ranking visitCount

--- controllers/rankController.js
+++ controllers/rankController.js
@@ -1,20 +1,24 @@
 import { connectionDB } from "../database/db.js";
 
 export async function getRanking(req, res) {
-
   try {
-    const ranking = (await connectionDB.query(`
+    const ranking = (
+      await connectionDB.query(`
     SELECT u.id, u.name, SUM("visitCount") AS "visitCount", COUNT("userId") AS "linksCount"
     FROM users u
     LEFT JOIN urls l ON u.id = l."userId"
     GROUP BY u.id
-    ORDER BY u.id ASC;`)).rows;
+    ORDER BY u.id ASC;`)
+    ).rows;
+
+    ranking.map((rank) => {
+      if (rank.visitCount === null) {
+        return (rank.visitCount = 0);
+      }
+    });
 
     res.status(200).send(ranking);
   } catch (error) {
     res.status(500).send(error.message);
   }
 }
-
-
-
